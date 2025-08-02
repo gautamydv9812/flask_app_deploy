c85307c2b099ec92268c3efcd45f37e4eedb7eaf
gautamydv9812 <gautamyadav9812102034@gmail.com>
2025-08-02 20:07:39 +0545

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
+@app.route('/userdata')
+def userdata():
+    return render_template('userdata.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
